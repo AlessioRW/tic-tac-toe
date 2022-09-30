228b79701938d0535ae18a712a5d66b576269516
Alessio Wright <alessiowright@outlook.com>
2022-09-30 14:52:25 +0100

    Added computer opponent to play against

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-#Tic-Tac-Toe
+# Tic-Tac-Toe
 Simple game of Tic-Tac-Toe to run on a web browser
--- main.js
+++ main.js
@@ -1,23 +1,66 @@
-let board = '012345678'
-let playsMade = 0
-let counters = ['X','O']
-let score = [0,0]
-let gameOver = false
-let funMode = false
-let botPlaying = false
-const statusText = document.getElementById('status')
-const resetButton = document.getElementById('reset-button')
-const scoreX = document.getElementById('score-X')
-const scoreY = document.getElementById('score-Y')
-const turnText = document.getElementById('turn')
-const body = document.getElementById('body')
-resetButton.hidden = true
+let board = '012345678';
+let playsMade = 0;
+let score = [0,0];
+let gameOver = false;
+let funMode = false;
+let botPlaying = false;
+const diffucltyCounter = 0;
+const counters = ['X','O'];
+const difficultyList = ['Easy','Hard'];
+document.getElementById('reset-button').hidden = true
 
 
-function checkWin(board){
-    for (i=0;i<2;i++){
-        counter = counters[i]
-        expression = new RegExp('\\bx{3}|.{3}x{3}.{3}|x{3}\\b|x.{2}x.{2}x|x.{3}x.{3}x|.{2}x.x.x.{2}'.replaceAll('x',counter))
+function getBestPath(currentBoard,move,movesTested){
+    var currentBoard = currentBoard.replace(move.toString(),['O','X'][movesTested%2]);
+    var pathScore = 0
+
+    var winner = checkBoard(currentBoard);
+    if (winner === 'O'){
+        pathScore -= 1;
+    }
+    else if (winner === 'X'){
+        pathScore += 1;
+    }
+
+    if ((new RegExp('[0-8]','g')).test(currentBoard) == false || movesTested > [0,10][diffucltyCounter%difficultyList.length]){ // board is full/Difficulty Reached
+        return pathScore
+    }
+
+    for (var x=0; x<9; x++){
+        if (currentBoard.includes(x)){
+            var nextPathScore = getBestPath(currentBoard,x,movesTested+1);
+            pathScore += nextPathScore;
+        }
+    }
+    return pathScore
+    
+}
+
+function botMove(){
+    let paths = [];
+    for (i = 0; i<9; i++){
+        if (board.includes(i)){
+            let virtualBoard = board; //define temporary board to the checks don't overwrite the real board
+            var pathScore = getBestPath(virtualBoard,i,0);
+            paths.push({pathNumber:i, score:pathScore})
+        }
+    }
+    let highestScore = Infinity
+    bestCell = -1
+    paths.forEach(function(path) {
+        if (path.score < highestScore){
+            highestScore = path.score
+            bestCell = path.pathNumber
+        }
+    });
+
+    return bestCell
+
+}
+function checkBoard(board){
+    for (var i=0;i<2;i++){
+        let counter = counters[i]
+        let expression = new RegExp('\\bx{3}|.{3}x{3}.{3}|x{3}\\b|x.{2}x.{2}x|x.{3}x.{3}x|.{2}x.x.x.{2}'.replaceAll('x',counter))
         if (expression.test(board)){
             return counter
         }
@@ -25,43 +68,58 @@ function checkWin(board){
     return false
 }
 
+function checkWin(){
+    winner = checkBoard(board)
+    if (winner != false){
+        document.getElementById('status').innerText = `${winner} has won!`
+        gameOver = true
+        document.getElementById('reset-button').hidden = false
+        updateScore(winner)
+        if (funMode){
+            for (i=0; i<9;i++){
+                cellElement = document.getElementById(i)
+                cellElement.style.backgroundImage = "url('images/clapping.gif')"
+            }
+        }
+        return
+    }
+    else if (playsMade == 9){
+        document.getElementById('status').innerText = 'It\s a draw!'
+        gameOver = true
+        document.getElementById('reset-button').hidden = false
+    }
+}
+
 function play(cell){
-    cellElement = document.getElementById(cell)
+
+    var cellElement = document.getElementById(cell)
     if (cellElement.innerText === '' && gameOver === false){
         cellElement.innerText = counters[playsMade%2]
         board = board.replace(cell,counters[playsMade%2])
         playsMade += 1
 
-        winner = checkWin(board)
-        if (winner != false){
-            statusText.innerText = `${winner} has won!`
-            gameOver = true
-            resetButton.hidden = false
-            updateScore(winner)
-            if (funMode){
-                for (i=0; i<9;i++){
-                    cellElement = document.getElementById(i)
-                    cellElement.style.backgroundImage = "url('images/clapping.gif')"
-                }
-            }
-
-        }
-        else if (playsMade == 9){
-            statusText.innerText = 'It\s a draw!'
-            gameOver = true
-            resetButton.hidden = false
-        }
+        checkWin()
     }
-    turnText.innerText = `Turn: ${counters[playsMade%2]}`
+
+    if (counters[playsMade%2] === 'O' && botPlaying === true){
+        bestCell = botMove()
+        var cellElement = document.getElementById(bestCell)
+        cellElement.innerHTML = 'O'
+        board = board.replace(bestCell,'O')
+        playsMade += 1
+        checkWin()
+        return
+    }
+    document.getElementById('turn').innerText = `Turn: ${counters[playsMade%2]}`
 }
 
 function reset(){
-    resetButton.hidden = true
+    document.getElementById('reset-button').hidden = true
     board = '012345678'
-    turnText.innerText = 'Turn: X'
+    document.getElementById('turn').innerText = 'Turn: X'
     playsMade = 0
     gameOver = false
-    statusText.innerText = ''
+    document.getElementById('status').innerText = ''
     for (i=0; i<9;i++){
         cellElement = document.getElementById(i)
         cellElement.innerText = ''
@@ -76,17 +134,17 @@ function updateScore(winner){
     else{
         score[1] += 1
     }
-    scoreX.innerText = `X: ${score[0]}`
-    scoreY.innerText = `O: ${score[1]}`
+    document.getElementById('score-X').innerText = `X: ${score[0]}`
+    document.getElementById('score-Y').innerText = `O: ${score[1]}`
 }
 
 function resetScore(){
     score = [0,0]
-    scoreX.innerText = `X: ${score[0]}`
-    scoreY.innerText = `O: ${score[1]}`
+    document.getElementById('score-X').innerText = `X: ${score[0]}`
+    document.getElementById('score-Y').innerText = `O: ${score[1]}`
 }
 
-function funButton(){
+function toggleFun(){
     if (funMode){
         funMode = false
         document.getElementById('fun-button').innerText = 'Fun Mode: Off'
@@ -95,4 +153,21 @@ function funButton(){
         funMode = true
         document.getElementById('fun-button').innerText = 'Fun Mode: On'
     }
+}
+
+
+function toggleBot(){
+    if (botPlaying){
+        botPlaying = false
+        document.getElementById('bot-button').innerText = 'Computer: Off'
+    }
+    else{
+        botPlaying = true
+        document.getElementById('bot-button').innerText = 'Computer: On'
+    }
+}
+
+function toggleDifficulty(){
+    diffucltyCounter += 1
+    document.getElementById('difficulty-button').innerText = `Difficulty:${difficultyList[diffucltyCounter%difficultyList.length]}`
 }
